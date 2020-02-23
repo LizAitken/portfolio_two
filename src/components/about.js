c32986677082fcd8fa3c5fce9d0640b231121f01
Liz Aitken <liz.a.aitken@gmail.com>
2020-02-23 15:52:10 -0500

    Added pdf

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,7 @@ import '../styles/about.css';
 import { IconContext } from "react-icons";
 import { GoFile, GoMail } from "react-icons/go";
 import { FaLinkedin } from "react-icons/fa";
+import Pdf from "../images/liz_aitken_resume_2020.pdf"
 
 const About = props => {
     const { scrollDown } = props;
@@ -18,7 +19,7 @@ const About = props => {
                         <br/> When I am not exploring the vast universe of software development, I am probably painting or adventuring the great outdoors with my husband and my pup. Feel free to check out my artist website as well, <a href="https://www.lizaitkenart.com/">www.lizaitkenart.com</a>.</p>     
                         <div className='icon-wrap'>
                             <IconContext.Provider value={{ className: 'react-icons' }}>
-                                <GoFile/>
+                                <a href={Pdf} target = "_blank"><GoFile/></a>
                                 <GoMail />
                                 <FaLinkedin />
                             </IconContext.Provider>
