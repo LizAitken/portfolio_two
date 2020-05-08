5f5be80e45a76791ba22ba9da9a04665e0f739d5
Liz Aitken <liz.a.aitken@gmail.com>
2020-05-08 13:10:16 -0400

    trying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { BrowserRouter as Router, Route } from "react-router-dom";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 import Homepage from './components/homePage';
@@ -8,10 +8,8 @@ import './App.css';
 
 function App() {
   return (
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
+      <Router basename={process.env.PUBLIC_URL}>       
           <Route path={process.env.PUBLIC_URL + '/'} exact render={Homepage}/>
-        </Switch>
       </Router>
   );
 }
