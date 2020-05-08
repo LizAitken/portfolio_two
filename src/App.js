a4863952ce41f499370f04e2f832fa1e7d3e1f59
Liz Aitken <liz.a.aitken@gmail.com>
2020-05-08 11:48:11 -0400

    Deleted unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import SoftwarePage from './components/softwarePage';
 import Homepage from './components/homePage';
 import './App.css';
 
@@ -9,7 +8,6 @@ function App() {
   return (
       <Router>
         <Route path='/' exact render={Homepage}/>
-        <Route path='/software-development' component={SoftwarePage}/>
       </Router>
   );
 }
