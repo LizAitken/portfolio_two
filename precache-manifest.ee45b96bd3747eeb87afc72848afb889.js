1d0da2144f19f2d63292e65eaa651b27675f407d
Liz Aitken <liz.a.aitken@gmail.com>
2020-05-09 02:16:51 -0400

    Updates

---
+++ precache-manifest.ee45b96bd3747eeb87afc72848afb889.js
@@ -0,0 +1,358 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04a8bd06a98e66b6bc0f3c8b64d2de18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6010c5890b6226502f3e",
+    "url": "/static/css/main.7011af1f.chunk.css"
+  },
+  {
+    "revision": "0d743ae377596ee63341",
+    "url": "/static/js/2.1b3efcdf.chunk.js"
+  },
+  {
+    "revision": "507c4436aa55e48136618e82a79cbf78",
+    "url": "/static/js/2.1b3efcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6010c5890b6226502f3e",
+    "url": "/static/js/main.90acea32.chunk.js"
+  },
+  {
+    "revision": "a8e62e0d607660759ee2",
+    "url": "/static/js/runtime-main.100184b4.js"
+  },
+  {
+    "revision": "d5c78fc58ccff8b655eb9b3d9bdef169",
+    "url": "/static/media/Bb_Headshot-7612.d5c78fc5.png"
+  },
+  {
+    "revision": "c1aa70762f499cfcdab378b8ac5baa87",
+    "url": "/static/media/blob-shape.c1aa7076.svg"
+  },
+  {
+    "revision": "1804d4750adbd3e5878bec7330267d42",
+    "url": "/static/media/bricks4schoolz.1804d475.png"
+  },
+  {
+    "revision": "eb05a070709917f83d9b6c884a394d8e",
+    "url": "/static/media/bs1.eb05a070.png"
+  },
+  {
+    "revision": "3325c3a36541c92aa76abc0d7cda3257",
+    "url": "/static/media/bs12.3325c3a3.png"
+  },
+  {
+    "revision": "cc94ec36e29bd0fb9e91d4b69e58ffd6",
+    "url": "/static/media/bs18.cc94ec36.png"
+  },
+  {
+    "revision": "771f00343f626db7f1c31ce09879c4ba",
+    "url": "/static/media/bs19.771f0034.png"
+  },
+  {
+    "revision": "95f490191ea15616efeafff0601d65c3",
+    "url": "/static/media/bs2.95f49019.png"
+  },
+  {
+    "revision": "611575a21fc5362ea22e397cea5ea0bb",
+    "url": "/static/media/bs20.611575a2.png"
+  },
+  {
+    "revision": "fc1aaf0ceb196188f446b3d970f004c0",
+    "url": "/static/media/bs21.fc1aaf0c.png"
+  },
+  {
+    "revision": "0671d5da384229d26a09dbccd8b4d6cf",
+    "url": "/static/media/bs3.0671d5da.png"
+  },
+  {
+    "revision": "41fd8d50b76d6d0d97b0b77b6ec99ef6",
+    "url": "/static/media/bs4.41fd8d50.png"
+  },
+  {
+    "revision": "f12548511944392bb000f34fa0f2156d",
+    "url": "/static/media/bs41.f1254851.png"
+  },
+  {
+    "revision": "41f5d8a30eb3004853742a4a27325bd7",
+    "url": "/static/media/bs5.41f5d8a3.png"
+  },
+  {
+    "revision": "bffd91622a96054808e9e4a0567605db",
+    "url": "/static/media/bs6.bffd9162.png"
+  },
+  {
+    "revision": "3dab3a88863186fb9d39d5b630817cff",
+    "url": "/static/media/build_icon@2x.3dab3a88.png"
+  },
+  {
+    "revision": "7ec7c171ccd66b624d732693fdda37b5",
+    "url": "/static/media/button_wave.7ec7c171.svg"
+  },
+  {
+    "revision": "4d4b7a6f5c2a72928b04dd6070037ee1",
+    "url": "/static/media/c1.4d4b7a6f.png"
+  },
+  {
+    "revision": "1c5b217c9d82f8d0f875c0075b7c82b5",
+    "url": "/static/media/c10.1c5b217c.png"
+  },
+  {
+    "revision": "a348074b8d2a0b25c4868f911f1e3e63",
+    "url": "/static/media/c2.a348074b.png"
+  },
+  {
+    "revision": "cc422a2187f49e9ee7ff9ad7d262bae2",
+    "url": "/static/media/c3.cc422a21.png"
+  },
+  {
+    "revision": "6710381a4292bbda98d17f9e91538c94",
+    "url": "/static/media/c4.6710381a.png"
+  },
+  {
+    "revision": "7fb42784b2570098b1b5555f2ff61498",
+    "url": "/static/media/c5.7fb42784.png"
+  },
+  {
+    "revision": "8636e83ca69137f9d26a2fcb8ae0f004",
+    "url": "/static/media/c6.8636e83c.png"
+  },
+  {
+    "revision": "a885c02b490a056b0deaa18c60030b21",
+    "url": "/static/media/c7.a885c02b.png"
+  },
+  {
+    "revision": "d8d66e52d3acb13e95f7f92f9c753dd8",
+    "url": "/static/media/c8.d8d66e52.png"
+  },
+  {
+    "revision": "57a54f726411b92d6092c692291f5d06",
+    "url": "/static/media/c9.57a54f72.png"
+  },
+  {
+    "revision": "8429cbefc3634984de8f31ff5ee346d3",
+    "url": "/static/media/cake.8429cbef.png"
+  },
+  {
+    "revision": "049d76baa78167e50ddb168d9d0168f9",
+    "url": "/static/media/comic2.049d76ba.png"
+  },
+  {
+    "revision": "a457d7f99d1f1657e0a6e913f689579d",
+    "url": "/static/media/create.a457d7f9.png"
+  },
+  {
+    "revision": "4f784f97dcd11114ead3ad4b9bbc8b92",
+    "url": "/static/media/dd_one.4f784f97.png"
+  },
+  {
+    "revision": "a9faa883226a682e6229d643cfa7ea01",
+    "url": "/static/media/dd_two.a9faa883.png"
+  },
+  {
+    "revision": "16ea150c31e8c15af27638345fbde66f",
+    "url": "/static/media/design_icon@2x.16ea150c.png"
+  },
+  {
+    "revision": "a85bc1579600340c6d7f942620249126",
+    "url": "/static/media/dog.a85bc157.png"
+  },
+  {
+    "revision": "6d9dde275d95e3b3e623d46d91798d5a",
+    "url": "/static/media/donate.6d9dde27.png"
+  },
+  {
+    "revision": "0ea19e4a6a557277107bf0d461d8d658",
+    "url": "/static/media/homeschooling.0ea19e4a.png"
+  },
+  {
+    "revision": "06c0cfdfe9b5cf9fc96c7c6b986641cd",
+    "url": "/static/media/liz_aitken_resume_2020.06c0cfdf.pdf"
+  },
+  {
+    "revision": "2b99a370fd2f707ad98f2b423902818e",
+    "url": "/static/media/m1.2b99a370.png"
+  },
+  {
+    "revision": "e4b9c9729ab35487176ca71bb17e1f2e",
+    "url": "/static/media/m10.e4b9c972.png"
+  },
+  {
+    "revision": "8fe6fa69b6ab6d274f53327e5edaf7be",
+    "url": "/static/media/m11.8fe6fa69.png"
+  },
+  {
+    "revision": "0e7055dc67d796a734df08e5e9a1ba07",
+    "url": "/static/media/m12.0e7055dc.png"
+  },
+  {
+    "revision": "4087d68ee9c67c9533b68dc1fd91f6b4",
+    "url": "/static/media/m13.4087d68e.png"
+  },
+  {
+    "revision": "7dd466ae9d6e1ef7c94f57c5058adb44",
+    "url": "/static/media/m14.7dd466ae.png"
+  },
+  {
+    "revision": "ed2c3d391c062dddfddedaf4b68f6307",
+    "url": "/static/media/m2.ed2c3d39.png"
+  },
+  {
+    "revision": "212d6333a04b4b887073dd3d29ff5e29",
+    "url": "/static/media/m3.212d6333.png"
+  },
+  {
+    "revision": "72e4e535933225691b8382b3ff51d432",
+    "url": "/static/media/m4.72e4e535.png"
+  },
+  {
+    "revision": "2353e26262066eb6a86fe794f21d2c86",
+    "url": "/static/media/m5.2353e262.png"
+  },
+  {
+    "revision": "b9a2dbafa8bc1724cfbac05a1eec2061",
+    "url": "/static/media/m6.b9a2dbaf.png"
+  },
+  {
+    "revision": "49bfe6ba93001617bd9f9885ed2616b3",
+    "url": "/static/media/m7.49bfe6ba.png"
+  },
+  {
+    "revision": "4c50f0c7d9a084a444888ebedc1b278e",
+    "url": "/static/media/m8.4c50f0c7.png"
+  },
+  {
+    "revision": "3752f413040e00771e24d810595a64a3",
+    "url": "/static/media/m9.3752f413.png"
+  },
+  {
+    "revision": "a1230f5a2717f666c69b92a2b76a8079",
+    "url": "/static/media/ocean_city.a1230f5a.png"
+  },
+  {
+    "revision": "c5a77d03505aec14cac6c38b46c65d5b",
+    "url": "/static/media/rabbit_hole.c5a77d03.png"
+  },
+  {
+    "revision": "de158c24fc88f55de84d256d24f011a0",
+    "url": "/static/media/relax.de158c24.png"
+  },
+  {
+    "revision": "1305674364bab1ffb032afde0fcab86e",
+    "url": "/static/media/scary_movies.13056743.png"
+  },
+  {
+    "revision": "a26b2f1bdc7949d04e44c6cc40597756",
+    "url": "/static/media/school.a26b2f1b.png"
+  },
+  {
+    "revision": "f137bffe6d974bf3cb8f3c6cfa0574f1",
+    "url": "/static/media/scp1.f137bffe.png"
+  },
+  {
+    "revision": "38ac96c5e68a6bed53f887de44730322",
+    "url": "/static/media/scp10.38ac96c5.png"
+  },
+  {
+    "revision": "147facfde4958f962db04835bc60be02",
+    "url": "/static/media/scp12.147facfd.png"
+  },
+  {
+    "revision": "792ea0557a36f3aafd24e48e54336116",
+    "url": "/static/media/scp15.792ea055.png"
+  },
+  {
+    "revision": "a923c042e11c0246dfd723f91cf6b2c9",
+    "url": "/static/media/scp2.a923c042.png"
+  },
+  {
+    "revision": "c8ec6ab4bc7b176a6a5fa8ac4cac23c0",
+    "url": "/static/media/scp3.c8ec6ab4.png"
+  },
+  {
+    "revision": "26e5c3ea0a06a6b2d7da337ba62a36fc",
+    "url": "/static/media/scp4.26e5c3ea.png"
+  },
+  {
+    "revision": "f57e96f5667ad511cf4a7cae8c136dc5",
+    "url": "/static/media/scp5.f57e96f5.png"
+  },
+  {
+    "revision": "f19e2d337db7ae620dec4d8f0c85a234",
+    "url": "/static/media/scp6.f19e2d33.png"
+  },
+  {
+    "revision": "5844892580e71acbf391ddbe2375b2e7",
+    "url": "/static/media/scp7.58448925.png"
+  },
+  {
+    "revision": "84f314f927db7d0b9a9e47da711b3d16",
+    "url": "/static/media/scp8.84f314f9.png"
+  },
+  {
+    "revision": "ef1feabfc5150f62562500022b4104c1",
+    "url": "/static/media/scp9.ef1feabf.png"
+  },
+  {
+    "revision": "bd39231e1a21f89f16cb0f0e3100ad32",
+    "url": "/static/media/shark_more.bd39231e.png"
+  },
+  {
+    "revision": "081f7a27ecc0a058446c440674134fa8",
+    "url": "/static/media/space37.081f7a27.png"
+  },
+  {
+    "revision": "e9d350f517a3de2ca9a0396341823af5",
+    "url": "/static/media/space40.e9d350f5.png"
+  },
+  {
+    "revision": "02b5e41973c94ca1e7de1711eba7cece",
+    "url": "/static/media/t1.02b5e419.png"
+  },
+  {
+    "revision": "e072b36910d83bc94cdf5f4ac0fed5f9",
+    "url": "/static/media/t10.e072b369.png"
+  },
+  {
+    "revision": "003456ceda4e9e45977a8ff460e31e63",
+    "url": "/static/media/t11.003456ce.png"
+  },
+  {
+    "revision": "68a4573c790654e3cfd80f9a842258ec",
+    "url": "/static/media/t2.68a4573c.png"
+  },
+  {
+    "revision": "a0e770ca988113f3c6254dce5ed29eb3",
+    "url": "/static/media/t3.a0e770ca.png"
+  },
+  {
+    "revision": "0e4a873f54f0f50e80f9e3a6f9c43ec0",
+    "url": "/static/media/t4.0e4a873f.png"
+  },
+  {
+    "revision": "e7162e83bbe0594311a639b4be1641d6",
+    "url": "/static/media/t5.e7162e83.png"
+  },
+  {
+    "revision": "ecfd77ca618c800376e7f6362df8032b",
+    "url": "/static/media/t6.ecfd77ca.png"
+  },
+  {
+    "revision": "f2f6394cee3355bc2e8ff885f3b7efac",
+    "url": "/static/media/t7.f2f6394c.png"
+  },
+  {
+    "revision": "3ccb26224380829d1f2a38b227f91bda",
+    "url": "/static/media/t8.3ccb2622.png"
+  },
+  {
+    "revision": "3bd2cc896aed8293ef79c5166e5394ab",
+    "url": "/static/media/t9.3bd2cc89.png"
+  },
+  {
+    "revision": "5aa6ed0c6d43d0bfa16f6886450a0e75",
+    "url": "/static/media/wave.5aa6ed0c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio_two/precache-manifest.8641601da412a30da437413f127f9480.js"
+  "/precache-manifest.ee45b96bd3747eeb87afc72848afb889.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_two/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
